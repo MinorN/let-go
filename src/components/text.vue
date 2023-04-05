bc08bdd2df60fad2b8be50130c6246a19abe531c
qijiajun <1481700559@qq.com>
2023-04-05 22:34:21 +0800

    first commit

---
+++ src/components/text.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        xxx
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/router/routes.ts
@@ -0,0 +1,11 @@
+import { RouteRecordRaw } from "vue-router"
+import Text from '../components/text.vue'
+
+const routers: RouteRecordRaw[] = [
+    {
+        path: '/',
+        component: Text
+    }
+]
+
+export default routers
